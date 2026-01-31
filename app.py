b44ca9ff859cc0d42fc84006dccdf858ff3c3daa
Logan <cjhuber102@mail.com>
2026-01-30 22:29:39 -0600

    set up evironment

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask
-from routes import register_routes
-
+from routes_public import register_routes
 
 app = Flask(__name__)
 
 
 
+register_routes(app)    
 
 if __name__ in '__main__':
     app.run(debug=True)
-    register_routes(app)    
-
---
+++ models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Admin(db.Model):
+    __tablename__ = 'admins'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
