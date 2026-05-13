fee12686ea1fa72c63c24f94bc267ecb98d06c59
Logan <cjhuber102@mail.com>
2026-05-13 17:10:28 -0500

    added dog gif, tooltips, swapped break image

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,2 +1,4 @@
-const navbar = document.querySelector('.main-navbar')
+// Triggering the tooltip on the dog gif in the navbar
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
